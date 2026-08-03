6d5074b6baa8924d164f0c2a56b1a7e96d147165
masterpham07 <masterpham07@gmail.com>
2026-08-03 10:14:05 +0700

    Xóa services

--- services/PaymentService.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <string>
-#include "models/User.h"
-
-class PaymentService {
-public:
-    // method: "wallet" hoặc "cash"
-    static bool pay(Rider& rider, Driver& driver, double amount, const std::string& method);
-};
